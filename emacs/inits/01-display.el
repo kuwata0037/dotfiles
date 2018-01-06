d6e05b11f22fe399d254b1a30b87b2e5192f27c8
kuwata0037 <kuwata0037@gmail.com>
2018-01-06 23:50:48 +0900

    refactor el-get-bundle into :init of use-package and change t to +1

--- emacs/inits/01-display.el
+++ emacs/inits/01-display.el
@@ -2,7 +2,7 @@
 ;;; Startup
 ;;;------------------------------
 ;;; Disbaled startup display
-(setq inhibit-startup-screen t)
+(setq inhibit-startup-screen +1)
 (setq initial-scratch-message "")
 
 ;;;------------------------------
@@ -11,7 +11,7 @@
 (use-package color)
 
 ;;; Color theme
-(load-theme 'wombat t)
+(load-theme 'wombat +1)
 ;; (el-get-bundle! badwolf-theme
 ;;   :type github
 ;;   :pkgname "bkruczyk/badwolf-emacs")
@@ -19,17 +19,20 @@
 ;;; Current line
 (defface my/hl-line-face
   '((((class color) (background dark))
-     (:background "NavyBlue" t))
+     (:background "NavyBlue" +1))
     (((class color) (background light))
-     (:background "LightGoldenrodYellow" t))
-    (t (:bold t)))
-    "hl-line's my face")
+     (:background "LightGoldenrodYellow" +1))
+    (t (:bold +1)))
+  "hl-line's my face")
 (setq hl-line-face 'my/hl-line-face)
 (global-hl-line-mode -1)
 
 ;;; Symbol
-(el-get-bundle! highlight-symbol
+(use-package highlight-symbol
+  :init
+  (el-get-bundle highlight-symbol)
   (add-hook 'prog-mode-hook 'highlight-symbol-mode)
+  :config
   (setq highlight-symbol-idle-delay 0.5))
 
 ;;; Parentheses
@@ -41,21 +44,24 @@
 (show-paren-mode)
 (setq show-paren-delay 0.3)
 
-(el-get-bundle rainbow-delimiters)
 (use-package rainbow-delimiters
-  :commands rainbow-delimiters-mode
-  :init     (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+  :init
+  (el-get-bundle rainbow-delimiters)
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
   :config
   (cl-loop for index from 1 to rainbow-delimiters-max-face-count do
-   (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-     (cl-callf color-saturate-name (face-foreground face) 30))))
+           (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+             (cl-callf color-saturate-name (face-foreground face) 30))))
 
 ;;; Trailing whitespace
-(setq-default show-trailing-whitespace t)
+(setq-default show-trailing-whitespace +1)
 (set-face-background 'trailing-whitespace "#B14770")
 
 ;;; Difference
-(el-get-bundle volatile-highlights
+(use-package volatile-highlights
+  :init
+  (el-get-bundle volatile-highlights)
+  :config
   (volatile-highlights-mode)
   ;; supprot undo-tree
   (vhl/define-extension 'undo-tree 'undo-tree-yany 'undo-tree-move)
@@ -83,6 +89,6 @@
 ;;; Minibuffer
 ;;;------------------------------
 (fset 'yes-or-no-p 'y-or-n-p)
-(setq completion-ignore-case t)
-(setq read-buffer-completion-ignore-case t)
-(setq read-file-name-completion-ignore-case t)
+(setq completion-ignore-case +1)
+(setq read-buffer-completion-ignore-case +1)
+(setq read-file-name-completion-ignore-case +1)
