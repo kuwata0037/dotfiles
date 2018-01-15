38e0901b2747850c6434ced8137a506f33d0562d
kuwata0037 <kuwata0037@gmail.com>
2018-01-15 23:45:28 +0900

    refactor 01-display.el

--- emacs/inits/01-display.el
+++ emacs/inits/01-display.el
@@ -1,13 +1,15 @@
-;;;------------------------------
-;;; Startup
-;;;------------------------------
+;;; 01-display.el --- UI configulation               -*- lexical-binding: t; -*-
+
+;;--------------------------------------------------
+;; Startup
+;;--------------------------------------------------
 ;;; Disbaled startup display
 (setq inhibit-startup-screen +1)
 (setq initial-scratch-message "")
 
-;;;------------------------------
-;;; Color
-;;;------------------------------
+;;--------------------------------------------------
+;; Color
+;;--------------------------------------------------
 (use-package color)
 
 ;;; Color theme
@@ -29,29 +31,16 @@
 
 ;;; Symbol
 (use-package highlight-symbol
-  :init
-  (el-get-bundle highlight-symbol)
-  (add-hook 'prog-mode-hook 'highlight-symbol-mode)
-  :config
-  (setq highlight-symbol-idle-delay 0.5))
+  :init (el-get-bundle highlight-symbol)
+  :hook (prog-mode . highlight-symbol-mode)
+  :config (setq highlight-symbol-idle-delay 0.5))
 
 ;;; Parentheses
-(electric-pair-mode)
-(electric-layout-mode)
-(add-to-list 'electric-pair-pairs '(?' . ?'))
-(add-to-list 'electric-pair-pairs '(?{ . ?}))
-
 (show-paren-mode)
 (setq show-paren-delay 0.3)
-
 (use-package rainbow-delimiters
-  :init
-  (el-get-bundle rainbow-delimiters)
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-  :config
-  (cl-loop for index from 1 to rainbow-delimiters-max-face-count do
-           (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-             (cl-callf color-saturate-name (face-foreground face) 30))))
+  :init (el-get-bundle rainbow-delimiters)
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 ;;; Trailing whitespace
 (setq-default show-trailing-whitespace +1)
@@ -59,17 +48,16 @@
 
 ;;; Difference
 (use-package volatile-highlights
-  :init
-  (el-get-bundle volatile-highlights)
+  :init (el-get-bundle volatile-highlights)
   :config
   (volatile-highlights-mode)
   ;; supprot undo-tree
-  (vhl/define-extension 'undo-tree 'undo-tree-yany 'undo-tree-move)
+  (vhl/define-extension 'undo-tree 'undo-tree-yank 'undo-tree-move)
   (vhl/install-extension 'undo-tree))
 
-;;;------------------------------
-;;; Frame
-;;;------------------------------
+;;--------------------------------------------------
+;; Frame
+;;--------------------------------------------------
 ;;; Bar
 (setq frame-title-format (format "%%f - Emacs@%s" (system-name)))
 (menu-bar-mode -1)
@@ -79,16 +67,18 @@
 
 ;;; Margin
 (global-linum-mode)
-(setq linum-format "%3d: ")
+(setq linum-format "%4d: ")
 
 ;;; Mode line
 (line-number-mode)
 (column-number-mode)
 
-;;;------------------------------
-;;; Minibuffer
-;;;------------------------------
+;;--------------------------------------------------
+;; Minibuffer
+;;--------------------------------------------------
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq completion-ignore-case +1)
 (setq read-buffer-completion-ignore-case +1)
 (setq read-file-name-completion-ignore-case +1)
+
+;;; 01-display.el ends here
