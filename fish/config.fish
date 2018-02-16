5cf1d2b6f391d349941ba5c7b0860fe929b0eecd
kuwata0037 <kuwata0037@gmail.com>
2018-02-16 22:58:46 +0900

    refactor environment variable setting using local_paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,44 +1,50 @@
 ########## env ##########
 
 if status --is-login
-    ### locale ###
+    ##### locale #####
     set -gx LANG en_US.UTF-8
 
-    ### path ###
-    set -g fish_user_paths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
+    ##### path #####
+    set -l local_paths /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
 
-    # for rust
-    if test -d $HOME/.cargo/bin
-        set fish_user_paths $HOME/.cargo/bin $fish_user_paths
+    # go
+    set -gx GOPATH $HOME/dev
+    if test -d $GOPATH/bin
+        set local_paths $GOPATH/bin $local_paths
     end
 
-    # for go
-    if test (which go)
-        set -gx GOPATH $HOME/dev
+    # ghq
+    set -gx GHQ_ROOT ~/dev/src
 
-        test -d $GOPATH/bin
-        and set -g fish_user_paths $GOPATH/bin $fish_user_paths
+    # rust
+    if test -d ~/.cargo/bin
+        set local_paths ~/.cargo/bin $local_paths
     end
 
-    set -gx GHQ_ROOT $GOPATH/src
-
-    # for pyenv
-    if test -d $HOME/.pyenv/bin
-        set fish_user_paths $HOME/.pyenv/bin $fish_user_paths
+    # pyenv
+    if test -d ~/.pyenv/bin
+        set local_paths ~/.pyenv/bin $local_paths
+    end
+    if which pyenv >/dev/null
+        set local_paths ~/.pyenv/shims $local_paths
         source (pyenv init - | psub)
     end
 
-    # for rbenv
-    if test -d $HOME/.rbenv/bin
-        set fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+    # rbenv
+    if test -d ~/.rbenv/bin
+        set local_paths ~/.rbenv/bin $local_paths
+    end
+    if which rbenv >/dev/null
+        set local_paths ~/.rbenv/shims $local_paths
         source (rbenv init - | psub)
     end
 
-    ### misc ###
-    set -gx TERM xterm-256color
+    # update PATH via fish_user_paths
+    set -g fish_user_paths $local_paths
 
-    # for tpm
-    set -gx TMUX_PLUGIN_MANAGER_PATH $HOME/.config/tmux/plugins/
+    ##### misc #####
+    set -gx TERM xterm-256color
+    set -gx TMUX_PLUGIN_MANAGER_PATH ~/.config/tmux/plugins/
 end
 
 ########## alias ##########
