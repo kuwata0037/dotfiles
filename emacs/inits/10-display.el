df0828a85efccb34a010fb112a8793e56dacae32
kuwata0037 <kuwata0037@gmail.com>
2018-06-24 23:28:14 +0900

    Fixed comments according to file name

--- emacs/inits/10-display.el
+++ emacs/inits/10-display.el
@@ -1,4 +1,4 @@
-;;; 01-display.el --- UI configulation               -*- lexical-binding: t; -*-
+;;; 10-display.el --- UI configulation               -*- lexical-binding: t; -*-
 
 ;;--------------------------------------------------
 ;; Startup
@@ -98,4 +98,4 @@
   :init (el-get-bundle spaceline)
   :config (spaceline-spacemacs-theme))
 
-;;; 01-display.el ends here
+;;; 10-display.el ends here
