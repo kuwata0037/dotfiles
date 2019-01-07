57ccc3ee086b8527e48b32df2ceb553c379b9dea
kuwata0037 <kuwata0037@gmail.com>
2019-01-07 19:24:16 +0900

    Change comment style

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
-########## alias ##########
+########################################
+### ALIAS
 
 alias e 'emacsclient -nw -a ""'
 alias ekill 'emacsclient -e "(kill-emacs)"'
@@ -6,39 +7,41 @@ alias g 'git'
 alias fzf 'fzf-tmux'
 
 
-########## env ##########
+########################################
+### ENVIRONMENT
 
-##### xdg #####
+# Xdg
 set -q XDG_CONFIG_HOME
 or set -l XDG_CONFIG_HOME ~/.config
 
-##### path #####
+# Path
 set -l local_paths /usr/local/{sbin,bin} /usr/{sbin,bin} /{sbin,bin}
 
-# go
+## go
 set -gx GOPATH $HOME/dev
 set local_paths $GOPATH/bin $local_paths
 
-# rust
+## rust
 set local_paths ~/.cargo/bin $local_paths
 
-# pyenv
+## pyenv
 set local_paths ~/.pyenv/{bin,shims} $local_paths
 
-# rbenv
+## rbenv
 set local_paths ~/.rbenv/{bin,shims} $local_paths
 
-# update PATH via fish_user_paths
+## update PATH via fish_user_paths
 set -g fish_user_paths $local_paths
 
-##### misc #####
+# Misc
 set -gx LANG en_US.UTF-8
 set -gx TERM xterm-256color
 set -gx GHQ_ROOT $GOPATH/src
 set -gx TMUX_PLUGIN_MANAGER_PATH $XDG_CONFIG_HOME/tmux/plugins/
 
 
-########## plugins ##########
+########################################
+### PLUGINS
 
 if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
