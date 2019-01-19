9f3a09416ed2955d5908c3106cfee96a7134c585
kuwata0037 <kuwata0037@gmail.com>
2019-01-19 22:54:06 +0900

    Enable preview mode when fzf find and open file

--- fish/config.fish
+++ fish/config.fish
@@ -37,8 +37,10 @@ set -gx TERM xterm-256color
 set -gx GHQ_ROOT $GOPATH/src
 set -gx TMUX_PLUGIN_MANAGER_PATH $XDG_CONFIG_HOME/tmux/plugins/
 set -gx FZF_DEFAULT_OPTS '--cycle --reverse --bind "?:toggle-preview"'
+set -gx FZF_FIND_FILE_OPTS '--preview "head -100 {}"'
 set -gx FZF_CD_OPTS '--preview "tree -C {} | head -n100"'
 set -gx FZF_CD_WITH_HIDDEN_OPTS $FZF_CD_OPTS
+set -gx FZF_ENABLE_OPEN_PREVIEW 1
 
 
 ########################################
