4546743e9f8f23e9becd58a9ea2189c67c8a05e5
kuwata0037 <kuwata0037@gmail.com>
2019-10-31 22:32:39 +0900

    Use GHQ_SELECTOR variables and remove alias of fzf-tmux

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,6 @@
 alias e 'emacsclient -nw -a ""'
 alias ekill 'emacsclient -e "(kill-emacs)"'
 alias g 'git'
-alias fzf 'fzf-tmux'
 
 
 ########################################
@@ -41,8 +40,9 @@ set -gx FZF_DEFAULT_OPTS '--cycle --reverse --bind "?:toggle-preview"'
 set -gx FZF_FIND_FILE_OPTS '--preview "head -100 {}"'
 set -gx FZF_CD_OPTS '--preview "tree -C {} | head -n100"'
 set -gx FZF_CD_WITH_HIDDEN_OPTS $FZF_CD_OPTS
+set -gx FZF_TMUX 1
 set -gx FZF_ENABLE_OPEN_PREVIEW 1
-
+set -gx GHQ_SELECTOR fzf-tmux
 
 ########################################
 ### PLUGINS
