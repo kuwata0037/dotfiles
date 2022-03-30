169e2b918c884c62eb55ac64fbe6d065ea7851bf
kuwata0037 <kuwata0037@gmail.com>
2022-03-30 22:59:05 +0900

    rewrite fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,56 +1,73 @@
-if status is-interactive
-    ########################################
-    ### ALIAS
+#
+# Alias
+#
+### Emacs
+alias e 'emacsclient -nw -a ""'
+alias ekill 'emacsclient -e "(kill-emacs)"'
 
-    alias e 'emacsclient -nw -a ""'
-    alias ekill 'emacsclient -e "(kill-emacs)"'
-    alias g git
-    alias gg 'ghq get'
+### Git
+alias g git
+alias gg 'ghq get'
 
-    ########################################
-    ### ENVIRONMENT
+### File
+if command -qs bat
+    alias cat bat
+end
 
-    # Xdg
-    set -q XDG_CONFIG_HOME
-    or set -l XDG_CONFIG_HOME ~/.config
+### Directory
+if command -qs exa
+    alias ls 'exa --icons'
+    alias ll 'exa --icons -lh'
+    alias la 'exa --icons -alh'
+end
+alias mk 'command mkdir -p'
 
-    # Path
-    set -l local_paths /usr/local/{sbin,bin} /usr/{sbin,bin} /{sbin,bin}
+#
+# Environment
+#
+### XDG
+set -q XDG_CONFIG_HOME
+or set -l XDG_CONFIG_HOME ~/.config
 
-    ## go
-    set -gx GOPATH $HOME/.go
-    set local_paths $GOPATH/bin $local_paths
+### Path
+set -l local_paths /usr/local/{sbin,bin} /usr/{sbin,bin} /{sbin,bin}
 
-    ## javascript
-    set -gx VOLTA_HOME $HOME/.volta
-    set local_paths $VOLTA_HOME/bin $local_paths
+# Go
+set -gx GOPATH $HOME/.go
+set local_paths $GOPATH/bin $local_paths
 
-    ## rust
-    set local_paths ~/.cargo/bin $local_paths
-    if type sccache 2>/dev/null
-        set -gx RUSTC_WRAPPER (which sccache)
-    end
+# JavaScript
+set -gx VOLTA_HOME $HOME/.volta
+set local_paths $VOLTA_HOME/bin $local_paths
 
-    ## update PATH via fish_user_paths
-    set -g fish_user_paths $local_paths
+# Python
+set -gx POETRY_VIRTUALENVS_IN_PROJECT true
 
-    # Misc
-    set -gx LANG en_US.UTF-8
-    set -gx TERM xterm-256color
-    set -gx TMUX_PLUGIN_MANAGER_PATH $XDG_CONFIG_HOME/tmux/plugins/
-    set -gx FZF_LEGACY_KEYBINDINGS 0
-    set -gx FZF_DEFAULT_OPTS '--cycle --reverse --bind "?:toggle-preview"'
-    set -gx FZF_FIND_FILE_OPTS '--preview "head -100 {}"'
-    set -gx FZF_CD_OPTS '--preview "tree -C {} | head -n100"'
-    set -gx FZF_CD_WITH_HIDDEN_OPTS $FZF_CD_OPTS
-    set -gx FZF_TMUX 1
-    set -gx FZF_ENABLE_OPEN_PREVIEW 1
-    set -gx GHQ_SELECTOR fzf-tmux
+# Rust
+set local_paths ~/.cargo/bin $local_paths
+if type sccache 2>/dev/null
+    set -gx RUSTC_WRAPPER (which sccache)
+end
 
-    ########################################
-    ### PLUGINS
+# update PATH via fish_user_paths
+set -g fish_user_paths $local_paths
 
-    if not functions -q fisher
-        curl -sL https://git.io/fisher | source && fisher update
-    end
+### FZF
+set -gx FZF_TMUX 1
+set -gx FZF_ENABLE_OPEN_PREVIEW 1
+set -gx FZF_DEFAULT_OPTS '--cycle --reverse --bind "?:toggle-preview"'
+set -gx FZF_FIND_FILE_OPTS '--preview "head -100 {}"'
+set -gx FZF_CD_OPTS '--preview "tree -C {} | head -n100"'
+set -gx FZF_CD_WITH_HIDDEN_OPTS $FZF_CD_OPTS
+
+### Misc
+set -gx LANG en_US.UTF-8
+set -gx TERM xterm-256color
+set -gx GHQ_SELECTOR fzf-tmux
+
+#
+# Plugins
+#
+if not functions -q fisher
+    curl -sL https://git.io/fisher | source && fisher update
 end
