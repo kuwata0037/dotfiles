8a7b9cf94e4abd0a295fb277446044bd375f59a9
kuwata0037 <kuwata0037@gmail.com>
2022-04-07 21:16:22 +0900

    install automatically homebrew on linux

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -2,9 +2,25 @@
 
 install_homebrew() {
     if !(type brew >/dev/null 2>&1); then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        if [ "$(uname)" == 'Darwin' ]; then
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            brew bundle --global
+        elif type apt-get >/dev/null 2>&1; then
+            sudo apt-get update
+            sudo apt-get install -y build-essential procps curl file git
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            test -d ~/.linuxbrew && ~/.linuxbrew/bin/brew bundle --global
+            test -d /home/linuxbrew/.linuxbrew && /home/linuxbrew/.linuxbrew/bin/brew bundle --global
+        elif type yum >/dev/null 2>&1; then
+            sudo yum groupinstall 'Development Tools'
+            sudo yum install -y procps-ng curl file git
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            test -d ~/.linuxbrew && ~/.linuxbrew/bin/brew bundle --global
+            test -d /home/linuxbrew/.linuxbrew && /home/linuxbrew/.linuxbrew/bin/brew bundle --global
+        else
+            echo "Unsupported OS. Please install manually."
+        fi
     fi
-    brew bundle --global
 }
 
 install_rust() {
